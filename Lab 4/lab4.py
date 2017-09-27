808dd7ee255c46ffe4b010cd7107fd8f4257b3a0
William Waters <wmwaters@umich.edu>
2017-09-27 18:50:23 -0400

    finish lab

--- Lab 4/lab4.py
+++ Lab 4/lab4.py
@@ -5,4 +5,9 @@ for line in file:
 	line = line.rstrip()
 	curlist = re.findall('^From.*', line)
 	if len(curlist) != 1 : continue
+	numberlist.append(curlist)
 	print(curlist)
+	curlist2 = re.findall('[0-9]+', line)
+	print(curlist2)
+	curlist3 = re.findall('From: (.*)@', line)
+	print(curlist3)
